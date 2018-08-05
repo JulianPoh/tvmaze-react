d99a42504d9c6206f5ebcf09e16b730b4dfb4c2f
JulianPoh <julianpoh01@gmail.com>
2018-08-05 16:38:55 +0800

    created starter files

---
+++ script.js
@@ -0,0 +1,11 @@
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>TVMaze React</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
