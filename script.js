7bd2ace998a039e9565306b5d4910d31c9a75530
JulianPoh <julianpoh01@gmail.com>
2018-08-05 21:38:37 +0800

    created title and search field

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-class Index extends React.Component {
+class Home extends React.Component {
+    
+      
+    state = {
+    word: ""
+    }
+    
+    clickHandler(){
+        console.log( "yay" );
+        }
+    
     render() {
         return (
-            <div>
-                <h1>TVMaze React</h1>
+            <div className="container">
+                <div>
+                    <h1>TVMaze React</h1>
+                    <input onChange={this.changeHandler} value={this.state.word}/>
+                    <button onClick={this.clickHandler}>Search</button>
+                </div>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+class Results extends React.Component {
+    render() {
+        var searchResults = this.props.queryText.map(results, index) => (
+            <Result key={index} name={show.show.name} img={show.show.image.medium} />
+        )
+        }
+    }
+}
+
+
+
+ReactDOM.render(<Home />, document.getElementById('root'));
